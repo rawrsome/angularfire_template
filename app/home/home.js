68fe3947b2ea1c77d72af077c18c7d09b7fefb02
Christopher Tran <christopher.tran@me.com>
2015-08-20 13:41:07 -0700

    Updated: able to recieve token from stripe

--- app/home/home.js
+++ app/home/home.js
@@ -1,24 +1,37 @@
 'use strict';
 
 // *inject firebase.js module from cdn
-var App = angular.module('myApp.home', ['ngRoute', 'firebase'])
-   // ====> Declared route 
-App.config(['$routeProvider', function($routeProvider) {
+var App = angular.module('myApp.home', ['ngRoute', 'firebase', 'angularPayments'])
+
+
+
+// ====> Declared route 
+App.config(['$routeProvider', function($routeProvider, $window) {
 
     $routeProvider.when('/home', {
        templateUrl: 'home/home.html',
        controller: 'HomeCtrl'
     });
+    Stripe.setPublishableKey('pk_test_ir3pQ7Xr2fy8TgUcYrDWXmkG');
 
 }])
+
+
 // ====> Home controller
 App.controller('HomeCtrl', [
    '$scope', '$rootScope',
    function($scope) {
       var ref = new Firebase('https://purestripe.firebaseio.com/');
-
+      // Stripe Response Handler
+		$scope.stripeCallback = function (code, result) {
+		    if (result.error) {
+		        window.alert('it failed! error: ' + result.error.message);
+		    } else {
+		        window.alert('success! token: ' + result.id);
+		    }
+		};
       
    }
+   
 ]);
-
 // <==== end Home controller
